0e36e4a6c697a8ac4d98ce2e98cd45bafedb5bf5
AldahirM <amvelasquez9621@gmail.com>
2024-10-24 10:26:41 -0500

    Init project

---
+++ src/main/java/com/co/fundmanagement/FundmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.co.fundmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundmanagementApplication.class, args);
+	}
+
+}
